48ee1739d8914dd2a826afca12626f8578df6285
Tomasz <oksiedz@gmail.com>
2022-01-16 12:09:39 +0100

    Added mouselistener for MyPanel and adding algorithm to find on which field on board user clicked

--- Java/szachy/src/Chess/MyPanel.java
+++ Java/szachy/src/Chess/MyPanel.java
@@ -14,7 +14,11 @@ import javax.swing.JPanel;
  *
  * @author ja
  */
-public class MyPanel extends JPanel {
+public class MyPanel extends JPanel implements MouseListener{//mouselsitener is an abstract listener
+
+    public MyPanel() {
+        addMouseListener(this); //we added the Panel as a tool to listen to mouse listener
+    }
 
     @Override
     public void paint(Graphics g) {
@@ -55,6 +59,50 @@ public class MyPanel extends JPanel {
                 ChessMainFrame.p2.getTab().get(i).drawChessman(g, b);
             }
         }
+    }
 
+    @Override
+    public void mouseClicked(MouseEvent e) { //e - event
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //System.out.println("X="+e.getX()+";Y="+e.getY());//returning location of click
+        //checking if the click was done on one of the figure
+        int x = e.getX();
+        int y = e.getY();
+        //assignment of field width/height on the board
+        int b; //variable to handle 1/8 of panel to paint the board
+            //setting the value of b
+            if (getWidth() > getHeight()) {
+                b = getHeight() / 8;
+            } else {
+                b = getWidth() / 8;
+            }
+        int cx = x/b; //number of column which we have clicked
+        int cy = y/b; //number of row which we have clicked
+        //System.out.println("X="+x+";cx="+cx+";Y="+y+";cy="+cy+";b="+b);//returning location of click
+        MyChessman ch = null;//creating object which will contain the figure on which we clicked
+        if (ChessMainFrame.p1 != null) {
+            //location we should divide by the b and check in the player container there is figure with such location
+        }
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
