f9090681b5dd0f5a0ad4c709353a8b874b5e76ed
Tomasz <oksiedz@gmail.com>
2022-05-29 21:17:52 +0200

    Added C# for Object Oriented Data base

---
+++ CSharp/ObjectOrientedDB/db4o-master/db4o-master/db4o/Auditor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db4o
+{
+	public class Auditor : User
+	{
+		public string position { get; set; }
+
+		void add_comment()
+		{
+			Console.WriteLine("Comment Added");
+		}
+
+		void add_ticket_label()
+		{
+			Console.WriteLine("Label Added");
+		}
+	}
+}
---
+++ CSharp/ObjectOrientedDB/db4o-master/db4o-master/db4o/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+namespace db4o
+{
+	public abstract class User
+	{
+		public string Full_name { get; set; };
+		public int ID { get; set; };
+		public string Login { get; set; };
+		private string Password { get; set; }
+
+		public void login()
+		{
+			Console.WriteLine("Logged in");
+		}
+
+		public void logout()
+		{
+			Console.WriteLine("Logged out");
+		}
+
+		public void open_menu()
+		{
+			Console.WriteLine("Menu opened");
+		}
+	}
