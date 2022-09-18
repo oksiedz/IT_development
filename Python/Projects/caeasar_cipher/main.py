22c8e43df3d74cf21709790d39ddcab3d954b080
Tomasz <oksiedz@gmail.com>
2022-09-18 21:41:28 +0200

    Caeasar cipher - encryption

---
+++ Python/Projects/caeasar_cipher/main.py
@@ -0,0 +1,36 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def encrypt(text, shift):
+
+    encrypted = ''
+    for letter in text:
+        new_letter = ''
+        letter_index = alphabet.index(letter)
+        if alphabet.index(letter) + shift > alphabet.index('z'):
+            new_letter = alphabet[(alphabet.index(letter) + shift) - alphabet.index('z') - 1]
+        else:
+            new_letter = alphabet[alphabet.index(letter) + shift]
+        encrypted += new_letter
+    print(f"Message {text} was encrypted to {encrypted}")
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+if direction == "encode":
+    encrypt(text,shift)
