ed998e9f4411aa9217852ab15b1bf0e99491b2cf
Tomasz <oksiedz@gmail.com>
2022-09-19 23:03:00 +0200

    Unification into one function

--- Python/Projects/caeasar_cipher/main.py
+++ Python/Projects/caeasar_cipher/main.py
@@ -1,38 +1,29 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-
-def encrypt(inputtext, inputshift):
-    encrypted = ''
-    for letter in inputtext:
-        new_letter = ''
-        alphabet.index(letter)
-        if alphabet.index(letter) + inputshift > alphabet.index('z'):
-            new_letter = alphabet[(alphabet.index(letter) + inputshift) - alphabet.index('z') - 1]
-        else:
-            new_letter = alphabet[alphabet.index(letter) + inputshift]
-        encrypted += new_letter
-    print(f"Message {inputtext} was encrypted to {encrypted}")
-
-
-def decrypt(inputtext, inputshift):
-    decrypted = ""
-    for letter in inputtext:
+def cesar_cipher(input_text, input_shift, input_direction):
+    output_word = ""
+    for letter in input_text:
         new_letter = ""
-        letter_index = alphabet.index(letter)
-        if alphabet.index(letter) - inputshift < 0:
-            new_letter = alphabet[(alphabet.index(letter) - inputshift)]
+        if input_direction == "encode":
+            if alphabet.index(letter) + input_shift > alphabet.index("z"):
+                new_letter = alphabet[(alphabet.index(letter) + input_shift) - alphabet.index('z') - 1]
+            else:
+                new_letter = alphabet[alphabet.index(letter) + input_shift]
         else:
-            new_letter = alphabet[alphabet.index(letter) - inputshift]
-        decrypted += new_letter
-    print(f"Message {inputtext} was decrypted to {decrypted}")
+            if alphabet.index(letter) - input_shift < alphabet.index("a"):
+                new_letter = alphabet[(alphabet.index(letter) - input_shift)]
+            else:
+                new_letter = alphabet[alphabet.index(letter) - input_shift]
+        output_word += new_letter
+    print(f"Message {input_text} was {input_direction}d to {output_word}")
 
 
-if direction == "encode":
-    encrypt(text, shift)
-if direction == "decode":
-    decrypt(text, shift)
+direction = ""
 
+while direction not in ["encode", "decode"]:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+if direction in ["encode", "decode"]:
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    cesar_cipher(input_text=text, input_shift=shift, input_direction=direction)
