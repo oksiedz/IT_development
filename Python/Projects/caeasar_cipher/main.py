a366e7c4210322496d34b0c05d5129c410454fa1
Tomasz <oksiedz@gmail.com>
2022-09-21 08:26:36 +0200

    handling other signs than letters (no conversion) + loop for the main program

--- Python/Projects/caeasar_cipher/main.py
+++ Python/Projects/caeasar_cipher/main.py
@@ -4,26 +4,39 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 def cesar_cipher(input_text, input_shift, input_direction):
     output_word = ""
     for letter in input_text:
-        new_letter = ""
-        if input_direction == "encode":
-            if alphabet.index(letter) + input_shift > alphabet.index("z"):
-                new_letter = alphabet[(alphabet.index(letter) + input_shift) - alphabet.index('z') - 1]
-            else:
-                new_letter = alphabet[alphabet.index(letter) + input_shift]
+        if letter not in alphabet:
+            output_word += letter
         else:
-            if alphabet.index(letter) - input_shift < alphabet.index("a"):
-                new_letter = alphabet[(alphabet.index(letter) - input_shift)]
+            new_letter = ""
+            if input_direction == "encode":
+                if alphabet.index(letter) + input_shift > alphabet.index("z"):
+                    new_letter = alphabet[(alphabet.index(letter) + input_shift) - alphabet.index('z') - 1]
+                else:
+                    new_letter = alphabet[alphabet.index(letter) + input_shift]
             else:
-                new_letter = alphabet[alphabet.index(letter) - input_shift]
-        output_word += new_letter
+                if alphabet.index(letter) - input_shift < alphabet.index("a"):
+                    new_letter = alphabet[(alphabet.index(letter) - input_shift)]
+                else:
+                    new_letter = alphabet[alphabet.index(letter) - input_shift]
+            output_word += new_letter
     print(f"Message {input_text} was {input_direction}d to {output_word}")
 
 
-direction = ""
 
-while direction not in ["encode", "decode"]:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-if direction in ["encode", "decode"]:
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    cesar_cipher(input_text=text, input_shift=shift, input_direction=direction)
+continue_loop = True
+
+while continue_loop:
+    direction = ""
+    while direction not in ["encode", "decode"]:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction in ["encode", "decode"]:
+        text = input("Type your message:\n").lower()
+        shift = (int(input("Type the shift number:\n")) % 26)
+
+        cesar_cipher(input_text=text, input_shift=shift, input_direction=direction)
+
+    continue_input = ""
+    while continue_input not in ["Yes", "No"]:
+        continue_input = input("Do you want to continue using Cesar Cipher? Yes/No\n")
+        if continue_input == "No":
+            continue_loop = False
