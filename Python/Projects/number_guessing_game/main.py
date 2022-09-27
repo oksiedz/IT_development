9bf3230b563dfcd36eb36b6356245c7d84af370d
Tomasz <oksiedz@gmail.com>
2022-09-27 20:16:36 +0200

    Python initial commit + graph

---
+++ Python/Projects/number_guessing_game/main.py
@@ -0,0 +1,13 @@
+# import from other files
+from graphics import logo
+
+print(logo)
+
+# Task list
+# 1. Computer selects the random number
+# 2. Set the difficulty level
+# 3. Guess the number and evaluate the result
+# 4. Repeat above till the end of repetition
+# 5. Check if number is the one which was selected
+# 6. Finish repetition when the selected number was guessed
+# 7. Start new/game, or finish
