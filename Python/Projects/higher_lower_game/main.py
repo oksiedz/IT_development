859e385da85c2171b0779e819135a7778c4fc9ad
Tomasz <oksiedz@gmail.com>
2022-09-29 18:10:10 +0200

    check how to proceed with list of dictionaries

--- Python/Projects/higher_lower_game/main.py
+++ Python/Projects/higher_lower_game/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from game_data import data
+from random import choice
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(data)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+new1 = choice(data)
+data.remove(new1)
+new2=new1['name']
+print(data)
+print(new1)
+print(new2)
