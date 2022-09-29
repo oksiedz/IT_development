82ea17dd88829273d0413ff01823365f2720c99b
Tomasz <oksiedz@gmail.com>
2022-09-29 21:39:03 +0200

    Writing tasks

--- Python/Projects/higher_lower_game/main.py
+++ Python/Projects/higher_lower_game/main.py
@@ -1,11 +1,25 @@
 from game_data import data
 from random import choice
+from os import system, name
 
-print(data)
 
-new1 = choice(data)
-data.remove(new1)
-new2=new1['name']
-print(data)
-print(new1)
-print(new2)
+
+
+# print(data)
+#
+# new1 = choice(data)
+# data.remove(new1)
+# new2=new1['name']
+# print(data)
+# print(new1)
+# print(new2)
+
+
+# print the logo and instruction
+# choose randomly first option
+# remove option from the main data list
+# choose randomly second option
+# ask for guess
+# evaluate guess - end of game, playing further
+# end game when the data list is empty
+# when end game - ask if player want's to play the new game
