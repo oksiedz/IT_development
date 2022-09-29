eb04983fe643c941f23ed498b95aa3a1c4fe1089
Tomasz <oksiedz@gmail.com>
2022-09-29 22:25:18 +0200

    Added documentation descriptions, two new functions to make code clearer and replace removing positions from data dictioanry to just going into random loop with comparisons. Also correcting the error with list where data were stored - before there will be an error due to not finnishing loop.

--- Python/Projects/higher_lower_game/main.py
+++ Python/Projects/higher_lower_game/main.py
@@ -4,6 +4,7 @@ from art import logo, vs
 
 
 def start_new_game():
+    """Function asks if player want to finish and close game or to start the new game"""
     answer = ""
     while answer not in ("yes", "no"):
         answer = input("Do you want to play new game? yes/no ").lower()
@@ -13,46 +14,55 @@ def start_new_game():
         return
 
 
+def choice_string(provided_choice):
+    """returns a string of choice"""
+    return f"{provided_choice['name']}, a {provided_choice['description']}, from {provided_choice['country']}"
+
+
+def correct_guess(answer, follower_1, follower_2):
+    """Function compares the answer with the correct results.
+    Return True or False"""
+    if follower_1 > follower_2:
+        return answer == "a"
+    elif follower_2 > follower_1:
+        return answer == 'b'
+    else:
+        False
+
+
 def higher_lower_game():
     score = 0
     the_end = 0
     first_choice = {}
     second_choice = {}
-    list_of_options = data
 
     while the_end == 0:
         if score == 0:
             first_choice = choice(data)
-            data.remove(first_choice)
         else:
             first_choice = second_choice
-
         second_choice = choice(data)
-        data.remove(second_choice)
+        while first_choice == second_choice:
+            second_choice = choice(data)
 
         print(logo)
         if score != 0:
             print(f"You are right! Current score: {score}")
-        print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}")
+        print(f"Compare A: {choice_string(first_choice)}")
         print(vs)
-        print(f"Against B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}")
+        print(f"Against B: {choice_string(second_choice)}")
         answer = ""
         while answer not in ("a", "b"):
             answer = input("Who has more followers? A or B: ").lower()
 
-        if answer == "a" and first_choice['follower_count'] > second_choice['follower_count'] or answer == "b" and first_choice['follower_count'] < second_choice['follower_count']:
+        if correct_guess(answer=answer, follower_1=first_choice['follower_count'],
+                         follower_2=second_choice['follower_count']):
             score += 1
         else:
             the_end = 1
 
-        if len(list_of_options) == 0:
-            the_end = 2
-
         if the_end == 1:
             print(f"Sorry that's wrong. Final score {score}")
-            start_new_game()
-        elif the_end == 2:
-            print(f"You answered correctly all comparisons. Bravo, your final score {score}")
             start_new_game()
 
 
