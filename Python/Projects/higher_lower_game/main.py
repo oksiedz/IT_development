a024aabfaeb0aa2082eb32388ae428f7abf164da
Tomasz <oksiedz@gmail.com>
2022-09-29 22:06:57 +0200

    working version

--- Python/Projects/higher_lower_game/main.py
+++ Python/Projects/higher_lower_game/main.py
@@ -1,25 +1,59 @@
 from game_data import data
 from random import choice
-from os import system, name
+from art import logo, vs
 
 
+def start_new_game():
+    answer = ""
+    while answer not in ("yes", "no"):
+        answer = input("Do you want to play new game? yes/no ").lower()
+    if answer == "yes":
+        higher_lower_game()
+    else:
+        return
 
 
-# print(data)
-#
-# new1 = choice(data)
-# data.remove(new1)
-# new2=new1['name']
-# print(data)
-# print(new1)
-# print(new2)
+def higher_lower_game():
+    score = 0
+    the_end = 0
+    first_choice = {}
+    second_choice = {}
+    list_of_options = data
+
+    while the_end == 0:
+        if score == 0:
+            first_choice = choice(data)
+            data.remove(first_choice)
+        else:
+            first_choice = second_choice
+
+        second_choice = choice(data)
+        data.remove(second_choice)
+
+        print(logo)
+        if score != 0:
+            print(f"You are right! Current score: {score}")
+        print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}")
+        print(vs)
+        print(f"Against B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}")
+        answer = ""
+        while answer not in ("a", "b"):
+            answer = input("Who has more followers? A or B: ").lower()
+
+        if answer == "a" and first_choice['follower_count'] > second_choice['follower_count'] or answer == "b" and first_choice['follower_count'] < second_choice['follower_count']:
+            score += 1
+        else:
+            the_end = 1
+
+        if len(list_of_options) == 0:
+            the_end = 2
+
+        if the_end == 1:
+            print(f"Sorry that's wrong. Final score {score}")
+            start_new_game()
+        elif the_end == 2:
+            print(f"You answered correctly all comparisons. Bravo, your final score {score}")
+            start_new_game()
 
 
-# print the logo and instruction
-# choose randomly first option
-# remove option from the main data list
-# choose randomly second option
-# ask for guess
-# evaluate guess - end of game, playing further
-# end game when the data list is empty
-# when end game - ask if player want's to play the new game
+higher_lower_game()
