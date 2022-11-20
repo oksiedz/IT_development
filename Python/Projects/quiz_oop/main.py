92c00151e37c6d7f6edce9f5808707b689d24f98
Tomasz <oksiedz@gmail.com>
2022-11-20 12:11:00 +0100

    Class QuizBrain and introduction of QuizBrain

--- Python/Projects/quiz_oop/main.py
+++ Python/Projects/quiz_oop/main.py
@@ -1,10 +1,11 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 # we need the list of question objects
 question_bank = []
 for question in question_data:
     # creation of list of objects are created from the list of dictionaries
     question_bank.append(Question(in_text=question["text"], in_answer=question["answer"]))
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+quiz.nex_question()
---
+++ Python/Projects/quiz_oop/quiz_brain.py
@@ -0,0 +1,16 @@
+#TODO: asking a question
+#TODO: checking if the answer is correct
+#TODO: checking if we're at the of the quiz
+
+# attributes - question_number = 0, questions_list
+# method - next question()
+
+class QuizBrain:
+    def __init__(self, in_question_list):
+        self.question_number = 0
+        self.questions_list = in_question_list
+
+    def nex_question(self):
+        current_question = self.questions_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question} (True/False)? ")
