33cc3f28293700ed38081f87588e6c9f7bd712bf
Tomasz <oksiedz@gmail.com>
2023-03-05 13:49:55 +0100

    Event listener

---
+++ Python/Projects/event_listeners/main.py
@@ -0,0 +1,55 @@
+import turtle as t
+
+tim = t.Turtle()
+screen = t.Screen()
+
+
+def move_forwards():
+    """turtle is moving 10 steps forward"""
+    tim.forward(10)
+
+
+def move_backwards():
+    """turtle is moving 10 steps back"""
+    tim.backward(10)
+
+
+def turn_cclockwise():
+    """turtle is turning counterclockwise (left) by 5 degrees"""
+    tim.left(5)
+
+
+def turn_clockwise():
+    """trutle is turning clockwise (right) by 5 degrees"""
+    tim.right(5)
+
+
+def clear_screen():
+    """function clearning drawings done by the turtle and returning it to state 0,0"""
+    tim.clearstamps()
+    tim.clear()
+    tim.setx(0)
+    tim.sety(0)
+    tim.clear()
+
+
+def ech_a_scetch():
+    """function responsible for Ech a sketch game
+    w - moves forward
+    s - moves backwards
+    a - turns left
+    d - turns right
+    c - clears the screen"""
+    screen.onkey(key="w", fun=move_forwards)
+    screen.onkey(key="s", fun=move_backwards)
+    screen.onkey(key="a", fun=turn_cclockwise)
+    screen.onkey(key="d", fun=turn_clockwise)
+    screen.onkey(key='c', fun=clear_screen)
+
+
+# below screen needs to start listening to events
+screen.listen()
+
+ech_a_scetch()
+
+screen.exitonclick()
