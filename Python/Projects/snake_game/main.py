699f1566f174ecc5089eeeaf5d20b05aba2bf088
ToOk <toksiedzki@deloittece.com>
2023-06-24 13:21:40 +0200

    Black screen with title

---
+++ Python/Projects/snake_game/main.py
@@ -0,0 +1,19 @@
+from turtle import Screen, Turtle
+
+# screen configuration
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("Snake game")
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
