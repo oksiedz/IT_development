f5f11bbec27dcbfe92bb850bb2e71850c8c87a6b
ToOk <toksiedzki@deloittece.com>
2023-06-24 13:30:32 +0200

    Snake created at the beginning of the game

--- Python/Projects/snake_game/main.py
+++ Python/Projects/snake_game/main.py
@@ -6,8 +6,15 @@ screen.setup(width = 600, height = 600)
 screen.bgcolor("black")
 screen.title("Snake game")
 
+snake = []
 
-
+# creation of first snake created from 3 squares
+for x in range(0, 3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.penup()
+    new_turtle.color("white")
+    new_turtle.goto(x * - 20, 0)
+    snake.append(new_turtle)
 
 
 
