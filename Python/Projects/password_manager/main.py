6706fdfb41c7555dc7d7d0a98734204ceb5a5615
Tomasz <oksiedz@gmail.com>
2024-03-29 09:46:26 +0100

    Adding function for password generator and additional logs for adding password

--- Python/Projects/password_manager/main.py
+++ Python/Projects/password_manager/main.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import messagebox
+import random
+
 
 
 def set_default_email_username():
@@ -8,7 +11,31 @@ def set_default_email_username():
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def password_generate():
-    print("password generated")
+    # Password Generator Project
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+
+    # concatenating symbols
+    password = "".join(password_list)
+
+    print(f"Your password is generated")
+
+    entry_password.delete(0, tk.END)
+    entry_password.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -18,21 +45,38 @@ def add_password():
     website = entry_website.get()
     email_username = entry_email_username.get()
     password = entry_password.get()
-    connection_string = f'{website} | {email_username} | {password}\n'
-    # open the file and check if the value already exists if not then add
-    with open(file="credentials.txt", mode="r") as file:
-        file_content = file.readlines()
-        if connection_string in file_content:
-            print("Such connection string already exists")
+
+    if len(website) == 0 or len(email_username) == 0 or len(password) == 0:
+        print("There are some empty values")
+        messagebox.showinfo(title="Entry error", message="Please do not leave any field empty!")
+    else:
+        print("All values are not empty")
+        # adding message box
+        save_confirmed = messagebox.askokcancel(title="Please confirm",
+                                                message=f'You entered below details:\n'
+                                                        f'Website: {website}\n'
+                                                        f'Email/Username: {email_username}\n'
+                                                        f'Password: {password}\n'
+                                                        f'Do you want to save them?')
+        if save_confirmed:
+            print("Password saving is confirmed")
+            connection_string = f'{website} | {email_username} | {password}\n'
+            # open the file and check if the value already exists if not then add
+            with open(file="credentials.txt", mode="r") as file:
+                file_content = file.readlines()
+                if connection_string in file_content:
+                    print("Such connection string already exists")
+                else:
+                    with open(file="credentials.txt", mode="a") as file_to_write:
+                        file_to_write.write(connection_string)
+                        print("Connection string saved")
+            # cleaning the entries
+            entry_website.delete(0, tk.END)
+            set_default_email_username()
+            entry_password.delete(0, tk.END)
+            entry_website.focus()
         else:
-            with open(file="credentials.txt", mode="a") as file_to_write:
-                file_to_write.write(connection_string)
-                print("Connection string saved")
-    # cleaning the entries
-    entry_website.delete(0, tk.END)
-    set_default_email_username()
-    entry_password.delete(0, tk.END)
-    entry_website.focus()
+            print("Password saving not confirmed")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
